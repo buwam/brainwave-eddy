c4a0201a1ccc864d2b96127caf94ebebd9b9e7ab
buwam <143486519+buwam@users.noreply.github.com>
2021-01-02 08:58:47 +0000

    add router

--- backend/main.py
+++ backend/main.py
@@ -8,4 +8,28 @@ app = FastAPI()
 
 graph = Graph()
 
-origins = ["http://localhost
+origins = ["http://localhost:3000", "http://localhost:8080"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/hello")
+def hello_view(name: str = "Pepals"):
+    return {"message": f"Hello there, {name}!"}
+
+
+def add_sentence(sentence: str):
+    if sentence == "":
+        raise HTTPException(status_code=404, detail="Sentence cannot be null.")
+    add_sentence_node(sentence, graph)
+    return graph.to_json()
+
+@app.get("/chunk")
+def process_sentence_chunk(chunk: str):
+    sentences = nltk.sen
