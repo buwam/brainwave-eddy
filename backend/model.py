e0ffa68a3ba340dd5941a8d9744ed2e9c29dbed1
buwam <143486519+buwam@users.noreply.github.com>
2021-01-02 11:48:03 +0000

    Create roadmap.md

--- backend/model.py
+++ backend/model.py
@@ -10,4 +10,24 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 def rotate_matrix(x, y, angle, x_shift=0, y_shift=0, units="DEGREES"):
- 
+    """
+    Rotates a point in the xy-plane counterclockwise through an angle about the origin
+    https://en.wikipedia.org/wiki/Rotation_matrix
+    :param x: x coordinate
+    :param y: y coordinate
+    :param x_shift: x-axis shift from origin (0, 0)
+    :param y_shift: y-axis shift from origin (0, 0)
+    :param angle: The rotation angle in degrees
+    :param units: DEGREES (default) or RADIANS
+    :return: Tuple of rotated x and y
+    """
+
+    # Shift to origin (0,0)
+    x = x - x_shift
+    y = y - y_shift
+
+    # Convert degrees to radians
+    if units == "DEGREES":
+        angle = math.radians(angle)
+
+    # Rotation 
