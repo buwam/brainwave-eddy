ea5d5694e5bb152223c63350e4470a35b8603723
buwam <143486519+buwam@users.noreply.github.com>
2022-11-05 13:05:05 +0000

    remove unused libs

--- frontend/convex/_generated/dataModel.js
+++ frontend/convex/_generated/dataModel.js
@@ -15,4 +15,12 @@ import { GenericId } from "convex/values";
  * An identifier for a document in Convex.
  *
  * Convex documents are uniquely identified by their `Id`, which is accessible
- * on the `_id` field. To learn more, see [Data Modeling](https://docs.convex
+ * on the `_id` field. To learn more, see [Data Modeling](https://docs.convex.dev/using/data-modeling).
+ *
+ * Documents can be loaded using `db.get(id)` in query and mutation functions.
+ *
+ * **Important**: Use `myId.equals(otherId)` to check for equality.
+ * Using `===` will not work because two different instances of `Id` can refer
+ * to the same document.
+ */
+export const Id = GenericId;
