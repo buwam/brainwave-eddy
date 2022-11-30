6fda62c5bc6a800e30716de52e1b309eaf4dea32
buwam <143486519+buwam@users.noreply.github.com>
2022-11-30 17:58:05 +0000

    Add .npmignore file

--- frontend/convex/mutations/addNode.js
+++ frontend/convex/mutations/addNode.js
@@ -4,3 +4,9 @@ export default mutation(async ({ db, args }) => {
   const { keyword, position, sentences, embedding } = args;
   const node = await db.create("Nodes", {
     keyword,
+    position,
+    sentences,
+    embedding,
+  });
+  return node;
+});
