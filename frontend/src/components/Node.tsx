5289882f2a2ad2ab868f529668f6aeefac31c8bd
buwam <143486519+buwam@users.noreply.github.com>
2022-12-22 15:23:51 +0000

    Update appspec.yml

--- frontend/src/components/Node.tsx
+++ frontend/src/components/Node.tsx
@@ -14,4 +14,28 @@ export const CustomNode = ({
   id,
 }: {
   data: CustomNodeData;
- 
+  id: string;
+}) => {
+  const [collapsed, setCollapsed] = useState(false);
+  const onChange = useCallback((evt: any) => {
+    console.log(evt.target.value);
+  }, []);
+
+  return (
+    <>
+      <Handle type="target" position={Position.Top} />
+      <Flex
+        width="175px"
+        className="Node"
+        cursor="pointer"
+        fontSize=".5rem"
+        textAlign="center"
+        flexDir="column"
+        textColor={THEME_COLORS.salmon}
+      >
+        <Flex
+          bgColor={THEME_COLORS.peach}
+          border={`1px solid ${THEME_COLORS.peach}`}
+          borderBottom="none"
+          fontWeight={600}
+          justifyContent="space-b
