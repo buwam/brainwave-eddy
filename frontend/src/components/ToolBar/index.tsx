054f3864d24d60de4f6583667f4cc44d1521c89c
buwam <143486519+buwam@users.noreply.github.com>
2022-12-28 22:37:43 +0000

    Version 0.12.0

--- frontend/src/components/ToolBar/index.tsx
+++ frontend/src/components/ToolBar/index.tsx
@@ -3,4 +3,35 @@ import { Link, useLocation } from "react-router-dom";
 import { Icon, VStack, Flex } from "@chakra-ui/react";
 import { THEME_COLORS } from "../../util";
 import { ArrowBackIcon, ChatIcon, InfoIcon } from "@chakra-ui/icons";
-import { GoListUnordered } f
+import { GoListUnordered } from "react-icons/go";
+import { NAVIGATION_STATE, useNavigation } from "../../hooks/useNavigation";
+
+interface NavLink {
+  link: string | null;
+  icon: React.ReactNode;
+  onClick: () => void;
+}
+
+export function ToolBar() {
+  const location = useLocation();
+  const { navigationState, setNavigationState } = useNavigation();
+
+  // Check if the current pathname is '/about'
+  const notFoundNavLinks: NavLink[] = [
+    {
+      link: "/",
+      icon: <ArrowBackIcon />,
+      onClick: () => {},
+    },
+  ];
+
+  const aboutNavLinks = [
+    {
+      link: "/",
+      icon: <ArrowBackIcon />,
+      onClick: () => {},
+    },
+  ];
+  const listNavLinks = [
+    {
+      
