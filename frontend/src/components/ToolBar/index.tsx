4efe40c5b2db93e680485823685d273cd520bcae
buwam <143486519+buwam@users.noreply.github.com>
2022-12-28 13:04:17 +0000

    Fix alignment

---
+++ frontend/src/components/ToolBar/index.tsx
@@ -0,0 +1,6 @@
+import React, { useContext } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { Icon, VStack, Flex } from "@chakra-ui/react";
+import { THEME_COLORS } from "../../util";
+import { ArrowBackIcon, ChatIcon, InfoIcon } from "@chakra-ui/icons";
+import { GoListUnordered } f
