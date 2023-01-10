fa9bafe0782194724f19a206eed2d6188ad3927b
buwam <143486519+buwam@users.noreply.github.com>
2023-01-10 22:11:12 +0000

    Update RunicAltarAutomation

--- frontend/src/convex/_generated/react.js
+++ frontend/src/convex/_generated/react.js
@@ -40,4 +40,21 @@ export const useQuery = useQueryGeneric;
  * [optimistic updates](https://docs.convex.dev/using/optimistic-updates).
  *
  * The value returned by this hook is stable across renders, so it can be used
- * by React dependency arrays and memoization logic r
+ * by React dependency arrays and memoization logic relying on object identity
+ * without causing rerenders.
+ *
+ * This relies on the {@link ConvexProvider} being above in the React component tree.
+ *
+ * @param name - The name of the mutation.
+ * @returns The {@link ReactMutation} object with that name.
+ */
+export const useMutation = useMutationGeneric;
+
+/**
+ * Construct a new {@link ReactAction}.
+ *
+ * Convex function objects can be called like functions to request execution of
+ * the corresponding Convex function.
+ *
+ * The value returned by this hook is stable across renders, so it can be used
+ * by React dependency arrays and memoization logic relying on object identit
