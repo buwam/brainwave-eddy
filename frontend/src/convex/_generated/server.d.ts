cc0586cda76856a913ca2e3c5e0ba96b6af36e69
buwam <143486519+buwam@users.noreply.github.com>
2023-01-14 19:44:31 +0000

    Update _masthead.scss

--- frontend/src/convex/_generated/server.d.ts
+++ frontend/src/convex/_generated/server.d.ts
@@ -20,3 +20,21 @@ import {
   QueryCtx as GenericQueryCtx,
   DatabaseReader as GenericDatabaseReader,
   DatabaseWriter as GenericDatabaseWriter,
+} from "convex/server";
+import type { DataModel } from "./dataModel.js";
+import type { API } from "./api.js";
+
+/**
+ * Define a query in this Convex app's public API.
+ *
+ * This function will be allowed to read your Convex database and will be accessible from the client.
+ *
+ * @param func - The query function. It receives a {@link QueryCtx} as its first argument.
+ * @returns The wrapped query. Include this as an `export` to name it and make it accessible.
+ */
+export declare const query: QueryBuilderForDataModel<DataModel>;
+
+/**
+ * Define a mutation in this Convex app's public API.
+ *
+ * This fun
