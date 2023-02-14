efd8b305b20be63e149ab342bf2b593a20525231
buwam <143486519+buwam@users.noreply.github.com>
2023-02-14 20:54:08 +0000

    some code clean up

---
+++ frontend/src/pages/AboutPage/Logo.tsx
@@ -0,0 +1,5 @@
+export default function Logo({ color = "#FF5B5B", alternateColor = "#FF5C5C"} : {color?: string, alternateColor?: string}) {
+  return (
+    <>
+      <g filter="url(#filter0_d_4_3)">
+        <circle cx="230" 
