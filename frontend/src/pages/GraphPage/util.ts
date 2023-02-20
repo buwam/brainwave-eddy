e94715cabfa65a28c45ec920f6f475509d558c4f
buwam <143486519+buwam@users.noreply.github.com>
2023-02-20 12:57:56 +0000

    Deleted unused class

--- frontend/src/pages/GraphPage/util.ts
+++ frontend/src/pages/GraphPage/util.ts
@@ -9,4 +9,28 @@ function findRootAndChildren(
 
   const childMap = new Map<MapNode, MapNode[]>();
   const parentSet = new Set<MapNode>();
-  for (const edge of edges)
+  for (const edge of edges) {
+    const edgeSourceNode = nodes.find((n) => n.id === edge.source);
+    const edgeTargetNode = nodes.find((n) => n.id === edge.target);
+    if (!childMap.has(edgeSourceNode!)) {
+      childMap.set(edgeSourceNode!, []);
+    }
+    childMap.get(edgeSourceNode!)!.push(edgeTargetNode!);
+    parentSet.add(edgeSourceNode!);
+  }
+  let root: MapNode;
+  for (const node of nodes) {
+    if (!parentSet.has(node)) {
+      root = node;
+      break;
+    }
+  }
+  return [root!, childMap];
+}
+
+function dfs(
+  node: MapNode,
+  childMap: Map<MapNode, MapNode[]>,
+  result: MapNode[]
+): void {
+  result.push(
