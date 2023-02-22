ebbfe1db31d03a3f5290d647d2492fa275b77d83
buwam <143486519+buwam@users.noreply.github.com>
2023-02-22 06:16:54 +0000

    simplify interface

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -2,4 +2,6 @@ import { Button, Card, Flex, Heading, Link } from "@chakra-ui/react";
 import { Layout } from "../components/Layout";
 import { THEME_COLORS } from "../util";
 
-con
+const ConfusedUngaBunga = () => (
+  <svg width="200" height="200" viewBox="0 0 393 392" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M264 11.5V23H231.02H198.04L184.402 38.898C176.9 47.642 170.763 55.113 170.763 55.5C170.763 55.887 176.9 63.358 184.402 72.102L198.04 88H204.02H210V109V130H204.12H198.241L184.62 145.968C177.129 154.75 171 162.368 171 162.897C171 163.427 176.919 170.754 184.154 179.18L197.307 194.5L230.404 195L263.5 195.5L263.759 247.25L264.018 299H176.509H89L88.988 295.25C88.956 285.031 83.791 273.703 75.441 265.539L69.264 259.5L73.098 256.784C75.206 255.29 
